2e414c8fb77913ea9c6495b3b99c80b97962f182
Vladislav <vlad22122@gmail.com>
2023-02-07 22:51:11 +0700

    RemoveComponentSystem added

---
+++ ECS/Systems/RemoveComponentSystem.cs
@@ -0,0 +1,19 @@
+﻿using DesertImage.ECS;
+
+namespace Game
+{
+    public class RemoveComponentSystem<T> : SystemBase, IReactEntityAddedSystem where T : IComponent, new()
+    {
+        public IMatcher Matcher { get; }
+
+        private ushort _componentId;
+        
+        public RemoveComponentSystem()
+        {
+            var component = ComponentsTool.GetInstanceFromPool<T>();
+            Matcher = Match.AllOf(component.Id);
+        }
+
+        public void Execute(IEntity entity) => entity.Remove(_componentId);
+    }
+}
